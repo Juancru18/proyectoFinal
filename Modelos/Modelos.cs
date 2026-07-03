2ae2a8d0ca5f77546f67935a4cebe94dfce34c0e
Tiago-Fernandez <fernandeztiago390@gmail.com>
2026-07-02 21:27:53 -0300

    se agrego la clase AlquilerBicicletas, en la carpeta Modelos

--- Modelos/Modelos.cs
+++ Modelos/Modelos.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace AlquilerBicicletas.Modelos
 {
-    internal class Modelos
+    internal class AlquilerBicicletas
     {
+        public int IdBici { get; set; }
+        public string Usuario { get; set; }
+
+        public DateTime HoraInicio { get; set; }
+        public decimal PrecioPorHora { get; set; }
+
+        public AlquilerBicicletas(int idBici, string usuario, decimal precioPorHora)
+        { 
+            IdBici = IdBici;
+            Usuario = usuario;
+            HoraInicio = DateTime.Now;
+            PrecioPorHora = precioPorHora;
+             
+        }
+        public override string ToString()
+        {
+            return $"Bici: {IdBici} | Usuario{Usuario} | Inicio: {HoraInicio} | Precio/Hora: {PrecioPorHora} ";
+        }
     }
 }
